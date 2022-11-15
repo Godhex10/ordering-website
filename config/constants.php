99920179025893e0e79a4999b98f1944f7f809c1
Godhex10 <108100749+Godhex10@users.noreply.github.com>
2022-11-16 00:22:29 +0100

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php 
+//start session
+session_start();
+
+//create constants to store non repeating values
+define('SITEURL', 'http://localhost/onlinefood/');
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'onlinefoodorder');
+$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //database connection
+$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //selecting Database
+?>
